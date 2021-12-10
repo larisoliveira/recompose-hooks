14c5b864495f29979c10d37da082ed0e7ebf44e7
Larissa Oliveira <larioli.me@gmail.com>
2021-12-10 09:30:28 -0300

    Adicionando novo item na lógica criada com recompose

--- src/pages/Recompose.jsx
+++ src/pages/Recompose.jsx
@@ -4,45 +4,52 @@ import { withState, withHandlers, compose } from 'recompose'
 //   updateValue(value+1)
 // }
 
-const Recompose = ({ value, value2, value3, onClick, onClick2 }) => { 
+const Recompose = ({ value1, value2, value3, onClick1, onClick2, onClick3 }) => { 
   // const onClick = () => updateValue(value+1)
   return (
     <div>
-      <button onClick={onClick}>Clique aqui para alterar teste</button>
-      <h1>Teste {value}</h1>
-      <button onClick={onClick2}>Clique aqui para alterar o valor do outro teste</button>
-      <h2>Outro teste {value2}</h2>
-      <h3>Outro teste do outro teste {value3}</h3>
+      <button onClick={onClick1}>Clique aqui para alterar a quantidade de tomates</button>
+      <p>{value1} &#127813;</p>
+      <button onClick={onClick2}>Clique aqui para alterar a quantidade de uvas</button>
+      <p>{value2} &#127815;</p>
+      <button onClick={onClick3}>Clique aqui para alterar a quantidade de melancias</button>
+      <p>{value3} &#127816;</p>
     </div>
   )
 }
 
-const state = withState('value',  'updateValue', 1)
-const state2 = withState('value2',  'updateValue2', 2)
-const state3 = withState('value3',  'updateValue3', 3)
+const state1 = withState('value1',  'updateValue1', 1)
+const state2 = withState('value2',  'updateValue2', 1)
+const state3 = withState('value3',  'updateValue3', 1)
 
 const handler1 = withHandlers({
-  onClick: ({ updateValue, value }) => () => {
-    updateValue(value+1)
+  onClick1: ({ updateValue1, value1 }) => () => {
+    updateValue1(value1+1)
   }
 })
 
 const handler2 = withHandlers({
-  onClick2: ({ updateValue2, value2, onClick }) => () => {
+  onClick2: ({ updateValue2, value2 }) => () => {
     updateValue2(value2+1)
-    onClick()
+  }
+})
+
+const handler3 = withHandlers({
+  onClick3: ({ updateValue3, value3 }) => () => {
+    updateValue3(value3+1)
   }
 })
 
 const states = compose(
-  state,
+  state1,
   state2,
   state3
 )
 
 const handlers = compose(
   handler1,
-  handler2
+  handler2,
+  handler3
 )
 
 const enhancer = compose(
